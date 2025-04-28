ee0499dab62f4dc1b41661827b4bf1a7c00fd6b9
BHC <oussamabhc@192.168.1.4>
2025-04-28 10:59:35 +0100

    add JPA repository interfaces for Customer, BankAccount, and AccountOperation

--- ebanking-backend/src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
+++ ebanking-backend/src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
@@ -1,7 +1,15 @@
 package ma.enset.ebankingbackend;
 
+import ma.enset.ebankingbackend.entities.Customer;
+import ma.enset.ebankingbackend.repositories.AccountOperationRepository;
+import ma.enset.ebankingbackend.repositories.BankAccountRepository;
+import ma.enset.ebankingbackend.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class EbankingBackendApplication {
@@ -10,4 +18,17 @@ public class EbankingBackendApplication {
         SpringApplication.run(EbankingBackendApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepository) {
+        return args -> {
+            Stream.of("Hassan", "Yassin", "Aicha").forEach(name -> {
+                Customer customer = new Customer();
+                customer.setName(name);
+                customer.setEmail(name + "@gmail.com");
+                customerRepository.save(customer);
+            });
+        };
+    }
 }
