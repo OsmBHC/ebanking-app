64b96875e806873084788e241feb37a388f0efbe
OsmBHC <oussamabouhouchpro@gmail.com>
2025-05-04 20:31:15 +0100

    test DAO layer with CommandLineRunner for data creation and retrieval

--- ebanking-backend/src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
+++ ebanking-backend/src/main/java/ma/enset/ebankingbackend/EbankingBackendApplication.java
@@ -1,6 +1,11 @@
 package ma.enset.ebankingbackend;
 
+import ma.enset.ebankingbackend.entities.AccountOperation;
+import ma.enset.ebankingbackend.entities.CurrentAccount;
 import ma.enset.ebankingbackend.entities.Customer;
+import ma.enset.ebankingbackend.entities.SavingAccount;
+import ma.enset.ebankingbackend.enums.AccountStatus;
+import ma.enset.ebankingbackend.enums.OperationType;
 import ma.enset.ebankingbackend.repositories.AccountOperationRepository;
 import ma.enset.ebankingbackend.repositories.BankAccountRepository;
 import ma.enset.ebankingbackend.repositories.CustomerRepository;
@@ -9,6 +14,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Date;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -28,6 +35,37 @@ public class EbankingBackendApplication {
                 customer.setName(name);
                 customer.setEmail(name + "@gmail.com");
                 customerRepository.save(customer);
+            });
+
+            customerRepository.findAll().forEach(customer -> {
+                CurrentAccount currentAccount = new CurrentAccount();
+                currentAccount.setId(UUID.randomUUID().toString());
+                currentAccount.setBalance(Math.random() * 90000);
+                currentAccount.setCreatedAt(new Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setCustomer(customer);
+                currentAccount.setOverDraft(9000);
+                bankAccountRepository.save(currentAccount);
+
+                SavingAccount savingAccount = new SavingAccount();
+                savingAccount.setId(UUID.randomUUID().toString());
+                savingAccount.setBalance(Math.random() * 90000);
+                savingAccount.setCreatedAt(new Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setCustomer(customer);
+                savingAccount.setInterestRate(5.5);
+                bankAccountRepository.save(savingAccount);
+            });
+
+            bankAccountRepository.findAll().forEach(bankAccount -> {
+                for (int i = 0; i < 10; i++) {
+                    AccountOperation accountOperation = new AccountOperation();
+                    accountOperation.setOperationDate(new Date());
+                    accountOperation.setAmount(Math.random() * 12000);
+                    accountOperation.setType(Math.random()>0.5 ? OperationType.DEBIT : OperationType.CREDIT);
+                    accountOperation.setBankAccount(bankAccount);
+                    accountOperationRepository.save(accountOperation);
+                }
             });
         };
     }
